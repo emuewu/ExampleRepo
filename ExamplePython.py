06650c44d823cec44f1957f9e712ce4bbd8208a1
emuewu <ewu0428@stu.neisd.net>
2024-10-15 09:27:42 -0700

    Update ExamplePython.py
    I added MORE spaces!

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,4 +1,6 @@
 
+
+
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
@@ -16,3 +18,4 @@ job_list = {'John': 'Doctor', 'Jane': 'Engineer', "Jim": 'Teacher'}
 
 for name, job in job_list.items():
     print(f"{name} is a {job}")
+
